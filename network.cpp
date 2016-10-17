e06779ecaf12f6a6e732ed56305c14f73812c04f
andrej1991 <ant.andrej@gmail.com>
2016-10-17 23:31:19 +0200

    changing the effective layets num adding stochastic gradient descent function

--- network.cpp
+++ network.cpp
@@ -8,19 +8,23 @@ Network::Network(int layers_num, int *layers)
 {
     try
     {
-        this->layers_num = layers_num;
+        this->total_layers_num = layers_num;
+        this->layers_num = layers_num - 1;
         this->layers = new int [layers_num];
         for(int i = 0; i < layers_num; i++)
             this->layers[i] = layers[i];
-        this->biases = new Matrice* [layers_num];
-        this->outputs = new Matrice* [layers_num];
-        this->weights = new Matrice* [layers_num];
-        for(int i = 0; i < layers_num; i++)
+        this->layers += 1;
+        this->biases = new Matrice* [this->layers_num];
+        this->outputs = new Matrice* [this->total_layers_num];
+        this->weights = new Matrice* [this->layers_num];
+        for(int i = 0; i < this->layers_num; i++)
             {
-                this->biases[i] = new Matrice(layers[i], 1);
-                this->outputs[i] = new Matrice(layers[i], 1);
-                this->weights[i] = new Matrice(layers[i], (i - 1) >= 0 ? layers[i - 1] : 1);
+                this->biases[i] = new Matrice(this->layers[i], 1);
+                this->outputs[i] = new Matrice(this->layers[i - 1], 1);
+                this->weights[i] = new Matrice(this->layers[i], this->layers[i - 1]);
             }
+        this->outputs[this->layers_num] = new Matrice(this->layers[this->layers_num - 1], 1);
+        this->outputs += 1;
     }
     catch(bad_alloc &ba)
         {
@@ -32,6 +36,7 @@ Network::Network(int layers_num, int *layers)
 
 Network::~Network()
 {
+    this->outputs -= 1;
     for(int i = 0; i < layers_num; i++)
         {
             delete this->biases[i];
@@ -41,6 +46,8 @@ Network::~Network()
     delete this->biases;
     delete this->outputs;
     delete this->weights;
+    this->layers -= 1;
+    delete this->layers;
 }
 
 void Network::initialize_biases()
@@ -52,10 +59,7 @@ void Network::initialize_biases()
         {
             for(int j = 0; j < this->layers[i]; j++)
                 {
-                    if(i == 0)
-                        val = 0;
-                    else
-                        random.read((char*)(&val), 2);
+                    random.read((char*)(&val), 2);
                     this->biases[i]->data[j][0] = val;
                     this->biases[i]->data[j][0] /= 63000;
                 }
@@ -72,13 +76,9 @@ void Network::initialize_weights()
         {
             for(int j = 0; j < this->layers[i]; j++)
                 {
-                    int len = this->get_inputlen(i);
-                    for(int k = 0; k < len; k++)
+                    for(int k = 0; k < this->layers[i - 1]; k++)
                         {
-                            if(i == 0)
-                                val = 1;
-                            else
-                                random.read((char*)(&val), 2);
+                            random.read((char*)(&val), 2);
                             this->weights[i]->data[j][k] = val;
                             this->weights[i]->data[j][k] /= 63000;
                         }
@@ -89,17 +89,19 @@ void Network::initialize_weights()
 
 inline void Network::layers_output(double **input, int layer)
 {
-    int inputlen = this->get_inputlen(layer);
     for(int i = 0; i < this->layers[layer]; i++)
         {
-            this->outputs[layer]->data[i][0] = this->neuron.sigmoid(this->weights[layer]->data[i], input, this->biases[layer]->data[i][0], inputlen);
+            this->outputs[layer]->data[i][0] = this->neuron.sigmoid(this->weights[layer]->data[i], input, this->biases[layer]->data[i][0], this->layers[layer - 1]);
         }
 }
 
 inline void Network::feedforward(double **input)
 {
-    this->layers_output(input, 0);
-    for(int i = 1; i < this->layers_num; i++)
+    for(int i = 0; i < this->layers[-1]; i++)
+        {
+            this->outputs[-1]->data[i][0] = input[i][0];
+        }
+    for(int i = 0; i < this->layers_num; i++)
         {
             this->layers_output(this->outputs[i - 1]->data, i);
         }
@@ -112,11 +114,10 @@ inline void Network::backpropagate(MNIST_data *trainig_data, Matrice **nabla_b, 
     Matrice cf_derivate = this->cost_derivate(this->outputs[this->layers_num - 1]->data, trainig_data->required_output);
     Matrice output_derivate = this->derivate_layers_output(this->layers_num - 1, this->outputs[this->layers_num - 2]->data);
     Matrice delta = hadamart_product(cf_derivate, output_derivate);
-    //Matrice delta;
     *(nabla_b[this->layers_num - 1]) = delta;
     *(nabla_w[this->layers_num - 1]) = delta * this->outputs[this->layers_num - 2]->transpose();
     /*passing backwards the error*/
-    for(int i = this->layers_num - 2; i > 0; i--)
+    for(int i = this->layers_num - 2; i >= 0; i--)
         {
             output_derivate = this->derivate_layers_output(i, this->outputs[i - 1]->data);
             multiplied = this->weights[i + 1]->transpose() * delta;
@@ -141,22 +142,11 @@ inline Matrice Network::derivate_layers_output(int layer, double **input)
     Matrice mtx(this->layers[layer], 1);
     for(int i = 0; i < this->layers[layer]; i++)
         {
-            mtx.data[i][0] = this->neuron.sigmoid_derivate(this->weights[layer]->data[i], input, this->biases[layer]->data[i][0], this->get_inputlen(layer));
+            mtx.data[i][0] = this->neuron.sigmoid_derivate(this->weights[layer]->data[i], input, this->biases[layer]->data[i][0], this->layers[layer - 1]);
         }
     return mtx;
 }
 
-inline int Network::get_inputlen(int layer)
-{
-    if(layer == 0)
-        {
-            return 1;
-        }
-    else
-        {
-            return this->layers[layer - 1];
-        }
-}
 
 void Network::update_weights_and_biasses(MNIST_data **training_data, int training_data_len, double learning_rate)
 {
@@ -172,15 +162,14 @@ void Network::update_weights_and_biasses(MNIST_data **training_data, int trainin
             dnw = new Matrice* [this->layers_num];
             for(int i = 0; i < this->layers_num; i++)
                 {
-                    int len = this->get_inputlen(i);
-                    w[i] = new Matrice(this->layers[i], len);
-                    dnw[i] = new Matrice(this->layers[i], len);
+                    w[i] = new Matrice(this->layers[i], this->layers[i - 1]);
+                    dnw[i] = new Matrice(this->layers[i], this->layers[i - 1]);
                     b[i] = new Matrice(this->layers[i], 1);
                     dnb[i] = new Matrice(this->layers[i], 1);
                     for(int j = 0; j < this->layers[i]; j++)
                         {
                             dnb[i]->data[j][0] = b[i]->data[j][0] = 0;
-                            for(int k = 0; k < len; k++)
+                            for(int k = 0; k < this->layers[i - 1]; k++)
                                 {
                                     dnw[i]->data[j][k] = w[i]->data[j][k] = 0;
                                 }
@@ -192,32 +181,28 @@ void Network::update_weights_and_biasses(MNIST_data **training_data, int trainin
             cerr<<"operator new failed"<<endl;
             return;
         }
-    //print_mtx_list(dnw, 3);
     for(int i = 0; i < training_data_len; i++)
         {
             this->backpropagate(training_data[i], dnb, dnw);
-            for(int j = 1; j < this->layers_num; j++)
+            for(int j = 0; j < this->layers_num; j++)
                 {
                     for(int k = 0; k < this->layers[j]; k++)
                         {
-                            int len = this->get_inputlen(j);
                             b[j]->data[k][0] += dnb[j]->data[k][0];
-                            for(int l = 0; l < len; l++)
+                            for(int l = 0; l < this->layers[j - 1]; l++)
                                 {
                                     w[j]->data[k][l] += dnw[j]->data[k][l];
                                 }
                         }
                 }
         }
-    //print_mtx_list(dnw, 3);
     double lr = learning_rate / training_data_len;
-    for(int i = 1; i < this->layers_num; i++)
+    for(int i = 0; i < this->layers_num; i++)
         {
             for(int j = 0; j < this->layers[i]; j++)
                 {
-                    int len = this->get_inputlen(i);
                     this->biases[i]->data[j][0] -= lr * b[i]->data[j][0];
-                    for(int k = 0; k < len; k++)
+                    for(int k = 0; k < this->layers[i - 1]; k++)
                         {
                             this->weights[i]->data[j][k] -= lr * w[i]->data[j][k];
                         }
@@ -236,6 +221,37 @@ void Network::update_weights_and_biasses(MNIST_data **training_data, int trainin
     delete[] dnb;
 }
 
+void Network::stochastic_gradient_descent(MNIST_data **training_data, int epochs, int epoch_len, double learning_rate, int trainingdata_len)
+{
+    MNIST_data **minibatch = new MNIST_data* [epoch_len];
+    int index;
+    ifstream random;
+    int bytes_to_read;
+    if(trainingdata_len < 256)
+        bytes_to_read = 1;
+    else if(trainingdata_len < 65536)
+        bytes_to_read = 2;
+    else if(trainingdata_len < 0x1000000)
+        bytes_to_read = 3;
+    else
+        bytes_to_read = 4;
+    random.open("/dev/urandom", ios::in);
+    for(int i = 0; i < epochs; i++)
+        {
+            for(int j = 0; j < epoch_len; j++)
+                {
+                    index = trainingdata_len + 1;
+                    while(index > trainingdata_len)
+                        {
+                            random.read((char*)(&index), bytes_to_read);
+                        }
+                    minibatch[j] = training_data[index];
+                }
+            this->update_weights_and_biasses(minibatch, epoch_len, learning_rate);
+        }
+    random.close();
+}
+
 
 void Network::test(MNIST_data **d)
 {
@@ -243,11 +259,9 @@ void Network::test(MNIST_data **d)
     for(int i = 0; i < 10; i++)
         cout << this->outputs[this->layers_num - 1]->data[i][0] << "   ";
     cout << endl;
-    for(int i = 0; i < 10; i++)
-        {
-            this->update_weights_and_biasses(&(d[i]), 100, 3);
-            //cout << "set" << i << "done" << "\n";
-        }
+
+    this->stochastic_gradient_descent(d,30,10,3);
+
     this->feedforward(d[8]->input);
     for(int i = 0; i < 10; i++)
         cout << this->outputs[this->layers_num - 1]->data[i][0] << "   ";
--- network.h
+++ network.h
@@ -6,7 +6,7 @@
 
 
 class Network{
-    int layers_num;
+    int total_layers_num, layers_num;
     int *layers;
     Matrice **biases, **outputs, **weights;
     void initialize_biases();
@@ -15,11 +15,11 @@ class Network{
     inline void backpropagate(MNIST_data *training_data, Matrice **nabla_b, Matrice **nabla_w);
     inline Matrice cost_derivate(double **output, double **required_output);
     inline Matrice derivate_layers_output(int layer, double **input);
-    inline int get_inputlen(int layer);
     void update_weights_and_biasses(MNIST_data **training_data, int training_data_len, double learning_rate);
     inline void feedforward(double **input);
     Neuron neuron;
     public:
+    void stochastic_gradient_descent(MNIST_data **training_data, int epochs, int epoch_len, double learning_rate, int trainingdata_len = 50000);
     Network(int layers_num, int *layers);
     ~Network();
     void test(MNIST_data **d);
