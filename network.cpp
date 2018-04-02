cde1f3c0f462d31d656d05d5cfc611078d72c529
andrej1991 <ant.andrej@gmail.com>
2018-04-02 23:10:10 +0200

    resolving merge conflicts

--- network.cpp
+++ network.cpp
@@ -10,42 +10,13 @@
 using namespace std;
 ///int layers_num, LayerDescriptor **layerdesc, int input_row, int input_col = 1, int costfunction_type = CROSS_ENTROPY_CF, bool dropout = false
 Network::Network(int layers_num, LayerDescriptor **layerdesc, int input_row, int input_col, int input_channel_count, int costfunction_type,  bool dropout):
-                dropout(dropout), input_row(input_row), input_col(input_col), input_channel_count(input_channel_count)
+                dropout(dropout), input_row(input_row), input_col(input_col), input_channel_count(input_channel_count), layers_num(layers_num)
 {
     try
     {
         this->costfunction_type = costfunction_type;
         this->total_layers_num = layers_num + 1;
-        this->layers_num = layers_num;
-        this->layers = new Layer* [this->total_layers_num];
-        Padding p;
-        if(layerdesc[0]->layer_type == FULLY_CONNECTED)
-            this->layers[0] = new InputLayer(input_row, 1, 1, SIGMOID, p, FULLY_CONNECTED);
-        else
-            this->layers[0] = new InputLayer(input_row, input_col, input_channel_count, SIGMOID, p, CONVOLUTIONAL);
-        this->layers += 1;
-        for(int i = 0; i < layers_num; i++)
-            {
-                switch(layerdesc[i]->layer_type)
-                {
-                case FULLY_CONNECTED:
-                    this->layers[i] = new FullyConnected(layerdesc[i]->neuron_count, this->layers[i - 1]->get_output_len(),
-                                                         layerdesc[i]->neuron_type);
-                    break;
-                case SOFTMAX:
-                    this->layers[i] = new Softmax(layerdesc[i]->neuron_count, this->layers[i - 1]->get_output_len());
-                    break;
-                case CONVOLUTIONAL:
-                    ///Convolutional(int input_row, int input_col, int input_channel_count, int kern_row, int kern_col, int map_count, int neuron_type, int next_layers_type, Padding &p, int stride=1)
-                    this->layers[i] = new Convolutional(this->layers[i - 1]->get_output_row(), this->layers[i - 1]->get_output_col(),
-                                                        this->layers[i - 1]->get_mapcount(), layerdesc[i]->row, layerdesc[i]->col,
-                                                        layerdesc[i]->mapcount, SIGMOID, layerdesc[i + 1]->layer_type, p, 1);
-                    break;
-                default:
-                    cerr << "Unknown layer type: " << layerdesc[i]->layer_type << "\n";
-                    throw std::exception();
-                }
-            }
+        this->construct_layers(layerdesc);
     }
     catch(bad_alloc &ba)
         {
@@ -53,14 +24,98 @@ Network::Network(int layers_num, LayerDescriptor **layerdesc, int input_row, int
         }
 }
 
+Network::Network(char *data)
+{
+    ///int layers_num, LayerDescriptor **layerdesc, int input_row, int input_col, int input_channel_count, int costfunction_type,  bool dropout
+    ///int layer_type, int neuron_type, int neuron_count, int col = 1, int mapcount = 1, int stride = 1
+    ifstream file (data, ios::in|ios::binary);
+    if(file.is_open())
+        {
+            int f_layer_type, f_neuron_type, f_neuron_count, f_col, f_mapcount, f_stride;
+            //LayerDescriptor **dsc = new LayerDescriptor* [this->layers_num];
+            LayerDescriptor *dsc[this->layers_num];
+            file.read(reinterpret_cast<char *>(&(this->layers_num)), sizeof(int));
+            file.read(reinterpret_cast<char *>(&(this->input_row)), sizeof(int));
+            file.read(reinterpret_cast<char *>(&(this->input_col )), sizeof(int));
+            file.read(reinterpret_cast<char *>(&(this->input_channel_count)), sizeof(int));
+            file.read(reinterpret_cast<char *>(&(this->costfunction_type)), sizeof(int));
+            file.read(reinterpret_cast<char *>(&(this->dropout)), sizeof(bool));
+            this->total_layers_num = this->layers_num + 1;
+            for(int i = 0; i < this->layers_num; i++)
+                {
+                    file.read(reinterpret_cast<char *>(&f_layer_type), sizeof(int));
+                    file.read(reinterpret_cast<char *>(&f_neuron_type), sizeof(int));
+                    file.read(reinterpret_cast<char *>(&f_neuron_count), sizeof(int));
+                    file.read(reinterpret_cast<char *>(&f_col), sizeof(int));
+                    file.read(reinterpret_cast<char *>(&f_mapcount), sizeof(int));
+                    file.read(reinterpret_cast<char *>(&f_stride), sizeof(int));
+                    dsc[i] = new LayerDescriptor(f_layer_type, f_neuron_type, f_neuron_count, f_col, f_mapcount, f_stride);
+                }
+            this->construct_layers(dsc);
+            for(int i = 0; i < this->layers_num; i++)
+                {
+                    this->layers[i]->load(file);
+                }
+            file.close();
+            for(int i = 0; i < this->layers_num; i++)
+                {
+                    delete dsc[i];
+                }
+        }
+    else
+        {
+            cerr << "Unable to open the file:" << '"' << file << '"' << endl;
+            throw exception();
+        }
+}
+
 Network::~Network()
 {
     this->layers -= 1;
-    for(int i = 0; i <= layers_num; i++)
+    for(int i = 0; i < this->layers_num; i++)
         {
             delete this->layers[i];
+            delete this->layerdsc[i];
         }
-    delete this->layers;
+    delete this->layers[this->layers_num];
+    delete[] this->layers;
+    delete[] this->layerdsc;
+}
+
+void Network::construct_layers(LayerDescriptor **layerdesc)
+{
+    this->layers = new Layer* [this->total_layers_num];
+    this->layerdsc = new LayerDescriptor* [this->layers_num];
+    Padding p;
+    if(layerdesc[0]->layer_type == FULLY_CONNECTED)
+        this->layers[0] = new InputLayer(input_row, 1, 1, SIGMOID, p, FULLY_CONNECTED);
+    else
+        this->layers[0] = new InputLayer(input_row, input_col, input_channel_count, SIGMOID, p, CONVOLUTIONAL);
+    this->layers += 1;
+    for(int i = 0; i < layers_num; i++)
+    {
+        this->layerdsc[i] = new LayerDescriptor(layerdesc[i]->layer_type, layerdesc[i]->neuron_type, layerdesc[i]->neuron_count,
+                                                layerdesc[i]->col, layerdesc[i]->mapcount, layerdesc[i]->stride);
+        switch(layerdesc[i]->layer_type)
+        {
+            case FULLY_CONNECTED:
+                this->layers[i] = new FullyConnected(layerdesc[i]->neuron_count, this->layers[i - 1]->get_output_len(),
+                                                     layerdesc[i]->neuron_type);
+                break;
+            case SOFTMAX:
+                this->layers[i] = new Softmax(layerdesc[i]->neuron_count, this->layers[i - 1]->get_output_len());
+                break;
+            case CONVOLUTIONAL:
+                ///Convolutional(int input_row, int input_col, int input_channel_count, int kern_row, int kern_col, int map_count, int neuron_type, int next_layers_type, Padding &p, int stride=1)
+                this->layers[i] = new Convolutional(this->layers[i - 1]->get_output_row(), this->layers[i - 1]->get_output_col(),
+                                                    this->layers[i - 1]->get_mapcount(), layerdesc[i]->row, layerdesc[i]->col,
+                                                    layerdesc[i]->mapcount, SIGMOID, layerdesc[i + 1]->layer_type, p, 1);
+                break;
+            default:
+                cerr << "Unknown layer type: " << layerdesc[i]->layer_type << "\n";
+                throw std::exception();
+        }
+    }
 }
 
 inline void Network::feedforward(Matrice **input)
@@ -105,9 +160,7 @@ double Network::cost(Matrice &required_output, int req_outp_indx)
 
 inline void Network::backpropagate(MNIST_data *trainig_data, Layers_features **nabla)
 {
-    ///TODO modfy this function based on multiple input features...
     ///currently the final layer has to be a clasification layer
-    ///TODO make some check if the final layer has only one output vector
     this->feedforward(trainig_data->input);
     Matrice **delta = new Matrice* [1];
     delta[0] = new Matrice;
@@ -214,44 +267,39 @@ inline void Network::add_back_removed_neurons(Matrice **w_bckup, Matrice **b_bck
     ;
 }
 
-void Network::load(char *filename)
-{
-    /*ifstream inp;
-    inp.open(filename, ios::in);
-    int f_layer, f_row, f_col;
-    double data;
-    Matrice *w, *b;
-    inp.read((char*)&f_layer, 4);
-    for(int i = 0; i < f_layer; i++)
-        {
-            inp.read((char*)&f_row, 4);
-            inp.read((char*)&f_col, 4);
-            w = new Matrice(f_row, f_col);
-            b = new Matrice(f_row, 1);
-            for(int j = 0; j < f_row; j++)
-                {
-                    for(int k = 0; k < f_col; k++)
-                        {
-                            inp.read((char*)&data, sizeof(double));
-                            w->data[j][k] = data;
-                        }
-                }
-            this->layers[i]->set_weights(w);
-            for(int l = 0; l < f_row; l++)
-                {
-                    inp.read((char*)&data, sizeof(double));
-                    b->data[l][0] = data;
-                }
-            this->layers[i]->set_biases(b);
-            delete w;
-            delete b;
-        }
-    inp.close();*/
-}
-
 void Network::store(char *filename)
 {
-    ;
+    ///(int layer_type, int neuron_type, int neuron_count, int col = 1, int mapcount = 1, int stride = 1)
+    ///int layers_num, LayerDescriptor **layerdesc, int input_row, int input_col, int input_channel_count, int costfunction_type,  bool dropout
+    ofstream network_params (filename, ios::out | ios::binary);
+    if(network_params.is_open())
+        {
+            network_params.write(reinterpret_cast<char *>(&(this->layers_num)), sizeof(int));
+            network_params.write(reinterpret_cast<char *>(&(this->input_row)), sizeof(int));
+            network_params.write(reinterpret_cast<char *>(&(this->input_col )), sizeof(int));
+            network_params.write(reinterpret_cast<char *>(&(this->input_channel_count)), sizeof(int));
+            network_params.write(reinterpret_cast<char *>(&(this->costfunction_type)), sizeof(int));
+            network_params.write(reinterpret_cast<char *>(&(this->dropout)), sizeof(bool));
+            for(int i=0; i<this->layers_num; i++)
+                {
+                    network_params.write(reinterpret_cast<char *>(&(this->layerdsc[i]->layer_type)), sizeof(int));
+                    network_params.write(reinterpret_cast<char *>(&(this->layerdsc[i]->neuron_type)), sizeof(int));
+                    network_params.write(reinterpret_cast<char *>(&(this->layerdsc[i]->neuron_count)), sizeof(int));
+                    network_params.write(reinterpret_cast<char *>(&(this->layerdsc[i]->col)), sizeof(int));
+                    network_params.write(reinterpret_cast<char *>(&(this->layerdsc[i]->mapcount)), sizeof(int));
+                    network_params.write(reinterpret_cast<char *>(&(this->layerdsc[i]->stride)), sizeof(int));
+                }
+            for(int i = -1; i < this->layers_num; i++)
+                {
+                    this->layers[i]->store(network_params);
+                }
+            network_params.close();
+        }
+    else
+        {
+            cerr << "Unable to open the file:" << '"' << filename << '"' << endl;
+            throw exception();
+        }
 }
 
 void Network::stochastic_gradient_descent(MNIST_data **training_data, int epochs, int minibatch_len, double learning_rate, bool monitor_learning_cost,
@@ -331,9 +379,51 @@ void Network::stochastic_gradient_descent(MNIST_data **training_data, int epochs
     rand.close();
 }
 
+void Network::check_accuracy(MNIST_data **test_data)
+{
+    int break_counter = 0;
+    int learning_accuracy, learnig_cost_counter = 0;
+    double learning_cost, previoius_learning_cost = 0;
+    Matrice helper(this->layers[this->layers_num - 1]->get_output_row(), 1);
+    Matrice output;
+    int test_data_len = 10000;
+    bool monitor_learning_cost = true;
+    for(int i = 0; i < this->layers[this->layers_num - 1]->get_output_row(); i++)
+        {
+            helper.data[i][0] == 0;
+        }
+    learning_accuracy = learning_cost = 0;
+    for(int j = 0; j < test_data_len; j++)
+        {
+            ///TODO this is an errorprone as well
+            output = this->get_output(test_data[j]->input);
+            if(getmax(output.data) == test_data[j]->required_output.data[0][0])
+                {
+                    learning_accuracy++;
+                }
+            if(monitor_learning_cost)
+                {
+                    //if(j > 0)
+                    //    helper.data[(int)test_data[j - 1]->required_output.data[0][0]][0] = 0;
+                    helper.data[(int)test_data[j]->required_output.data[0][0]][0] = 1;
+                    learning_cost += this->cost(helper, test_data[j]->required_output.data[0][0]);
+                    helper.data[(int)test_data[j]->required_output.data[0][0]][0] = 0;
+                }
+        }
+    cout << learning_accuracy << " out of: " << test_data_len << endl;
+    if(monitor_learning_cost)
+        {
+            cout << "total cost: " << learning_cost << endl;
+            if(abs(learning_cost) > abs(previoius_learning_cost))
+                learnig_cost_counter++;
+            previoius_learning_cost = learning_cost;
+        }
+}
+
 void Network::test(MNIST_data **d, MNIST_data **v)
 {
     ///(MNIST_data **training_data, int epochs, int minibatch_len, double learning_rate, bool monitor_learning_cost, double regularization_rate, MNIST_data **test_data, int minibatch_count, int test_data_len, int trainingdata_len)
     this->stochastic_gradient_descent(d, 2, 10, 0.1, true, 10, v, 50);
+    this->store("/home/andrej/myfiles/Asztal/net.bin");
     //this->get_output(v[0]->input);
 }
