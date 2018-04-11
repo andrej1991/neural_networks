537396503acc9542a4e38ee04efed95d776f9790
andrej1991 <ant.andrej@gmail.com>
2018-04-11 22:03:26 +0200

    first commit

--- main.cpp
+++ main.cpp
@@ -32,30 +32,16 @@ int main()
         }
     cout << "the training data and the validation data is loaded\n";
     LayerDescriptor *layers[4];
-    layers[0] = new LayerDescriptor(CONVOLUTIONAL, SIGMOID, 4, 6, 5);
-    layers[1] = new LayerDescriptor(CONVOLUTIONAL, SIGMOID, 7, 5, 5);
-    layers[2] = new LayerDescriptor(FULLY_CONNECTED, SIGMOID, 30);
+    layers[0] = new LayerDescriptor(CONVOLUTIONAL, RELU, 5, 5, 7);
+    layers[1] = new LayerDescriptor(CONVOLUTIONAL, RELU, 5, 5, 7);
+    layers[2] = new LayerDescriptor(FULLY_CONNECTED, RELU, 30);
     layers[3] = new LayerDescriptor(SOFTMAX, SIGMOID, 10);
     //Network n(2, layers, 784, 1, 1, LOG_LIKELIHOOD_CF);
     Network n(4, layers, 28, 28, 1, LOG_LIKELIHOOD_CF);
     n.test(m, validation);
     input.close();
     required_output.close();
-    /*double inp[5][5] = {{2,7,5,3,6},{4,9,8,6,3},{7,5,4,6,8},{1,0,7,4,2},{9,4,5,6,1}};
-    Matrice input(5,5);
-    for(int i = 0; i<5; i++)
-        for(int j = 0; j<5; j++)
-            input.data[i][j] = inp[i][j];
-    double kern[3][3] = {{4,7,2},{1,5,9},{8,5,2}};
-    Matrice kernel(3,3);
-    for(int i = 0; i<3; i++)
-        for(int j = 0; j<3; j++)
-            kernel.data[i][j] = kern[i][j];
-    Matrice output1(3,3);
-    Matrice output2(3,3);
-    convolution(input,kernel,output1);
-    print_mtx(output1);
-    conv(input,kernel,output2);
-    print_mtx(output2);*/
+    Network n2("/home/andrej/myfiles/Asztal/net.bin");
+    n2.check_accuracy(validation);
     return 0;
 }
