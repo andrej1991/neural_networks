3c52eb110606896554b0228abc3128a4fd3b7552
andrej1991 <ant.andrej@gmail.com>
2021-02-06 22:24:35 +0100

    writing some unittests to test the convolution, maybe some more are needed for full coverage

--- unittests/unittests.cpp
+++ unittests/unittests.cpp
@@ -327,6 +327,172 @@ void test_transpose_pass()
     ASSERT_EQ(true, test_matrixes_for_equality(Result, ExpectedResult));
 }
 
+void test_convolution_positive1()
+{
+    Matrix Input(7,8), Kernel(2,2), Result(6, 7), ExpectedResult(6,7);
+    double helper1[7][8] = {{1, 2, 3, 4, 5, 6, 7, 8},
+                            {9, 10, 11, 12, 13, 14, 15, 16},
+                            {17, 18, 19, 20, 21, 22, 23, 24},
+                            {25, 26, 27, 28, 29, 30, 31, 32},
+                            {33, 34, 35, 36, 37, 38, 39, 40},
+                            {41, 42, 43, 44, 45, 46, 47, 48},
+                            {49, 50, 51, 52, 53, 54, 55, 56}};
+    double helper2[2][2] = {{1.1, 2.2},
+                            {3.3, 4.4}};
+    double helper3[6][7] = {{41.8, 52.8, 63.8, 74.8, 85.8, 96.8, 107.8},
+                            {129.8, 140.8, 151.8, 162.8, 173.8, 184.8, 195.8},
+                            {217.8, 228.8, 239.8, 250.8, 261.8, 272.8, 283.8},
+                            {305.8, 316.8, 327.8, 338.8, 349.8, 360.8, 371.8},
+                            {393.8, 404.8, 415.8, 426.8, 437.8, 448.8, 459.8},
+                            {481.8, 492.8, 503.8, 514.8, 525.8, 536.8, 547.8}};
+    for (int row=0; row<7; row++)
+    {
+        for (int col=0; col < 8; col++)
+        {
+            Input.data[row][col] = helper1[row][col];
+        }
+    }
+    for(int row=0; row<2; row++)
+    {
+        for(int col=0; col<2; col++)
+        {
+            Kernel.data[row][col] = helper2[row][col];
+        }
+    }
+    for(int row=0; row<6; row++)
+    {
+        for(int col=0; col<7; col++)
+        {
+            ExpectedResult.data[row][col] = helper3[row][col];
+        }
+    }
+    convolution(Input, Kernel, Result, 1, 1);
+    ASSERT_EQ(true, test_matrixes_for_equality(Result, ExpectedResult));
+}
+
+void test_convolution_positive1_with_strides()
+{
+    Matrix Input(7,8), Kernel(2,2), Result(3, 4), ExpectedResult(3,4);
+    double helper1[7][8] = {{1, 2, 3, 4, 5, 6, 7, 8},
+                            {9, 10, 11, 12, 13, 14, 15, 16},
+                            {17, 18, 19, 20, 21, 22, 23, 24},
+                            {25, 26, 27, 28, 29, 30, 31, 32},
+                            {33, 34, 35, 36, 37, 38, 39, 40},
+                            {41, 42, 43, 44, 45, 46, 47, 48},
+                            {49, 50, 51, 52, 53, 54, 55, 56}};
+    double helper2[2][2] = {{1.1, 2.2},
+                            {3.3, 4.4}};
+    double helper3[6][7] = {{41.8, 63.8, 85.8, 107.8},
+                            {217.8, 239.8, 261.8, 283.8},
+                            {393.8, 415.8, 437.8, 459.8}};
+    for (int row=0; row<7; row++)
+    {
+        for (int col=0; col < 8; col++)
+        {
+            Input.data[row][col] = helper1[row][col];
+        }
+    }
+    for(int row=0; row<2; row++)
+    {
+        for(int col=0; col<2; col++)
+        {
+            Kernel.data[row][col] = helper2[row][col];
+        }
+    }
+    for(int row=0; row<3; row++)
+    {
+        for(int col=0; col<4; col++)
+        {
+            ExpectedResult.data[row][col] = helper3[row][col];
+        }
+    }
+    convolution(Input, Kernel, Result, 2, 2);
+    ASSERT_EQ(true, test_matrixes_for_equality(Result, ExpectedResult));
+}
+
+void test_cross_correlation_positive1()
+{
+    Matrix Input(7,8), Kernel(2,2), Result(6, 7), ExpectedResult(6,7);
+    double helper1[7][8] = {{1, 2, 4, 8, 16, 32, 64, 128},
+                            {3, 6, 9, 12, 18, 21, 24, 27},
+                            {2, 3, 5, 7, 11, 13, 17, 19},
+                            {256, 512, 1024, 2048, 4096, 8192, 16384, 32768},
+                            {30, 33, 36, 39, 42, 45, 48, 51},
+                            {23, 29, 31, 37, 41, 47, 53, 59},
+                            {49, 50, 51, 52, 53, 54, 55, 56}};
+    double helper2[2][2] = {{1.1, 2.2},
+                            {3.3, 4.4}};
+    double helper3[6][7] = {{41.8, 70.4, 104.5, 162.8, 239.8, 350.9, 550},
+                            {36.3, 58.3, 83.6, 124.3, 159.5, 193.6, 225.5},
+                            {3106.4, 6209.5, 12411.3, 24812.7, 49602.3, 99174.9, 198307},
+                            {1652.2, 3083.3, 5922.4, 11577.5, 22864.6, 45415.7, 90494.8},
+                            {309.1, 347.6, 390.5, 437.8, 487.3, 543.4, 599.5},
+                            {470.8, 489.5, 512.6, 535.7, 561, 588.5, 616}};
+    for (int row=0; row<7; row++)
+    {
+        for (int col=0; col < 8; col++)
+        {
+            Input.data[row][col] = helper1[row][col];
+        }
+    }
+    for(int row=0; row<2; row++)
+    {
+        for(int col=0; col<2; col++)
+        {
+            Kernel.data[row][col] = helper2[row][col];
+        }
+    }
+    for(int row=0; row<6; row++)
+    {
+        for(int col=0; col<7; col++)
+        {
+            ExpectedResult.data[row][col] = helper3[row][col];
+        }
+    }
+    cross_correlation(Input, Kernel, Result, 1, 1);
+    ASSERT_EQ(true, test_matrixes_for_equality(Result, ExpectedResult));
+}
+
+void test_cross_correlation_positive1_with_strides()
+{
+    Matrix Input(7,8), Kernel(2,2), Result(3, 4), ExpectedResult(3,4);
+    double helper1[7][8] = {{1, 2, 4, 8, 16, 32, 64, 128},
+                            {3, 6, 9, 12, 18, 21, 24, 27},
+                            {2, 3, 5, 7, 11, 13, 17, 19},
+                            {256, 512, 1024, 2048, 4096, 8192, 16384, 32768},
+                            {30, 33, 36, 39, 42, 45, 48, 51},
+                            {23, 29, 31, 37, 41, 47, 53, 59},
+                            {49, 50, 51, 52, 53, 54, 55, 56}};
+    double helper2[2][2] = {{1.1, 2.2},
+                            {3.3, 4.4}};
+    double helper3[3][4] = {{41.8, 104.5, 239.8, 550},
+                            {3106.4, 12411.3, 49602.3, 198307},
+                            {309.1, 390.5, 487.3, 599.5}};
+    for (int row=0; row<7; row++)
+    {
+        for (int col=0; col < 8; col++)
+        {
+            Input.data[row][col] = helper1[row][col];
+        }
+    }
+    for(int row=0; row<2; row++)
+    {
+        for(int col=0; col<2; col++)
+        {
+            Kernel.data[row][col] = helper2[row][col];
+        }
+    }
+    for(int row=0; row<3; row++)
+    {
+        for(int col=0; col<4; col++)
+        {
+            ExpectedResult.data[row][col] = helper3[row][col];
+        }
+    }
+    cross_correlation(Input, Kernel, Result, 2, 2);
+    ASSERT_EQ(true, test_matrixes_for_equality(Result, ExpectedResult));
+}
+
 
 TEST(MatrixTest, BasicOperationsPass)
 {
@@ -339,6 +505,9 @@ TEST(MatrixTest, BasicOperationsPass)
     test_mtx_plus_equal_double_pass();
     test_hadamart_pass();
     test_transpose_pass();
+    test_convolution_positive1();
+    //test_cross_correlation_positive1_with_strides();
+    //test_convolution_positive1_with_strides();
 }
 
 
@@ -346,4 +515,43 @@ int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
+    /*Matrix Input(7,8), Kernel(2,2), Result(6, 7), ExpectedResult(6,7);
+    double helper1[7][8] = {{1, 2, 4, 8, 16, 32, 64, 128},
+                            {3, 6, 9, 12, 18, 21, 24, 27},
+                            {2, 3, 5, 7, 11, 13, 17, 19},
+                            {256, 512, 1024, 2048, 4096, 8192, 16384, 32768},
+                            {30, 33, 36, 39, 42, 45, 48, 51},
+                            {23, 29, 31, 37, 41, 47, 53, 59},
+                            {49, 50, 51, 52, 53, 54, 55, 56}};
+    double helper2[2][2] = {{1.1, 2.2},
+                            {3.3, 4.4}};
+    double helper3[6][7] = {{41.8, 70.4, 104.5, 162.8, 239.8, 350.9, 550},
+                            {36.3, 58.3, 83.6, 124.3, 159.5, 193.6, 225.5},
+                            {3106.4, 6209.5, 12411.3, 24812.7, 49602.3, 99174.9, 198307},
+                            {1652.2, 3083.3, 5922.4, 11577.5, 22864.6, 45415.7, 90494.8},
+                            {309.1, 347.6, 390.5, 437.8, 487.3, 543.4, 599.5},
+                            {470.8, 489.5, 512.6, 535.7, 561, 588.5, 616}};
+    for (int row=0; row<7; row++)
+    {
+        for (int col=0; col < 8; col++)
+        {
+            Input.data[row][col] = helper1[row][col];
+        }
+    }
+    for(int row=0; row<2; row++)
+    {
+        for(int col=0; col<2; col++)
+        {
+            Kernel.data[row][col] = helper2[row][col];
+        }
+    }
+    for(int row=0; row<6; row++)
+    {
+        for(int col=0; col<7; col++)
+        {
+            ExpectedResult.data[row][col] = helper3[row][col];
+        }
+    }
+    cross_correlation(Input, Kernel, Result, 2, 2);
+    print_mtx(Result);*/
 }
